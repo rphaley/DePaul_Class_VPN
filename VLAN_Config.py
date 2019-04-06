8612da43a9f3146451f8fc46eea90a25ee5e3a90
rphaley <rhaley@mail.depaul.edu>
2019-04-05 20:31:36 -0500

    update vlan output

--- VLAN_Config.py
+++ VLAN_Config.py
@@ -1,17 +1,44 @@
-def makeVlan(classNum, inNet,vlan,users):
-    f = open('VLANconfig.txt','a')
+def makeLayer3Vlan(classNum, inNet,vlan,users):
+    f = open('VLAN_Layer3_config.txt','a')
     for i in range(1,users):
         if i <10:
-            f.write('''vlan {}0{}
-name CNS{}_STUDNET{}_VLAN
-int vlan {}0{}
-ip address 10.{}.{}.254 255.255.255.0
-no shut\n!\n!\n'''.format(vlan,i,classNum,i,vlan,i,inNet,i))
+            f.write('''vlan {0}0{1}
+name CNS{2}_STUDNET{1}_VLAN
+access-list 2{0}0{1} remark CNS{2}_STUDNET{1}_ACL
+access-list 2{0}0{1} permit ip any 10.{3}.{1}.0 0.0.0.255
+access-list 2{0}0{1} deny ip any 10.0.0.0 0.255.255.255
+access-list 2{0}0{1} permit ip any any
+int vlan {0}0{1}
+description CNS{2}_STUDNET{1}
+ip address 10.{3}.{1}.254 255.255.255.0
+no shut
+ip access-group 2{0}0{1} in
+router ospf 1000
+network 10.1{0}.0.0 0.0.255.255 area 0\n!\n!\n'''.format(vlan,i,classNum,inNet))
         else:
-            f.write('''vlan {}{}
-name CNS{}_STUDNET{}_VLAN
-int vlan {}{}
-ip address 10.{}.{}.254 255.255.255.0
-no shut\n!\n!\n'''.format(vlan,i,classNum,i,vlan,i,inNet,i))
+            f.write('''vlan {0}{1}
+name CNS{2}_STUDNET{1}_VLAN
+access-list 2{0}{1} remark CNS{2}_STUDNET{1}_ACL
+access-list 2{0}{1} permit ip any 10.{3}.{1}.0 0.0.0.255
+access-list 2{0}{1} deny ip any 10.0.0.0 0.255.255.255
+access-list 2{0}{1} permit ip any any
+int vlan {0}{1}
+description CNS{2}_STUDNET{1}
+ip address 10.{3}.{1}.254 255.255.255.0
+no shut
+ip access-group 2{0}{1} in\n!\n!\n'''.format(vlan,i,classNum,inNet))
             
     f.close()
+
+def makeLayer2Vlan(classNum, inNet,vlan,users):
+    f = open('VLAN_Layer2_config.txt','a')
+    for i in range(1,users):
+        if i <10:
+            f.write('''vlan {0}0{1}
+name CNS{2}_STUDNET{1}_VLAN\n!\n!\n'''.format(vlan,i,classNum,inNet))
+        else:
+            f.write('''vlan {0}{1}
+name CNS{3}_STUDNET{1}_VLAN\n!\n!\n'''.format(vlan,i,classNum,inNet))
+            
+    f.close()
+
