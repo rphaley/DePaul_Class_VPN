6238cff0bb2b08e588eb0553b500820dfbe47602
rphaley <rhaley@mail.depaul.edu>
2019-04-05 21:25:02 -0500

    vlan bug fixes

--- VLAN_Config.py
+++ VLAN_Config.py
@@ -13,8 +13,9 @@ description CNS{2}_STUDNET{1}
 ip address 10.{3}.{1}.254 255.255.255.0
 no shut
 ip access-group 2{0}0{1} in
+access-list MGMT_VPN_TUNNEL_LIST extended permit ip object SSL_CNS{2}_NET object ADMIN_SSL_NET
 router ospf 1000
-network 10.1{0}.0.0 0.0.255.255 area 0\n!\n!\n'''.format(vlan,i,classNum,inNet))
+network 10.1{1}.0.0 0.0.255.255 area 0\n!\n!\n'''.format(vlan,i,classNum,inNet))
         else:
             f.write('''vlan {0}{1}
 name CNS{2}_STUDNET{1}_VLAN
@@ -38,7 +39,7 @@ def makeLayer2Vlan(classNum, inNet,vlan,users):
 name CNS{2}_STUDNET{1}_VLAN\n!\n!\n'''.format(vlan,i,classNum,inNet))
         else:
             f.write('''vlan {0}{1}
-name CNS{3}_STUDNET{1}_VLAN\n!\n!\n'''.format(vlan,i,classNum,inNet))
+name CNS{2}_STUDNET{1}_VLAN\n!\n!\n'''.format(vlan,i,classNum,inNet))
             
     f.close()
 
